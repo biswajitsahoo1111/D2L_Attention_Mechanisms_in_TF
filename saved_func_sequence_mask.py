664957b00d19a42463e630a73cdd626791a65a72
Biswajit Sahoo <biswajitsahoo1111@gmail.com>
2021-05-25 07:46:30 +0530

    ready to be pushed to d2l repo

--- saved_func_sequence_mask.py
+++ saved_func_sequence_mask.py
@@ -2,8 +2,15 @@ import tensorflow as tf
 
 def sequence_mask(X, valid_len, value = 0):
     """Mask irrelevant entries in sequences.
-    Input argument X of this function is 2D.
+    Argument:
+        X: either a 2D or 3D tensor
+        valid_len: 1D tensor
+        value: value to be substitued for mask
     """
     maxlen = X.shape[1]
     mask = tf.range(start = 0, limit = maxlen, dtype = tf.float32)[None, :] < tf.cast(valid_len[:, None], dtype = tf.float32)
-    return tf.where(mask, X, value)
+    
+    if len(X.shape) == 3:
+        return tf.where(tf.expand_dims(mask, axis = -1), X, value)
+    else:
+        return tf.where(mask, X, value)
